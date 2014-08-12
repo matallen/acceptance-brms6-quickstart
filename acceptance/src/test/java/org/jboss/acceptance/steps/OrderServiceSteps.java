938cb723d62ff632f2a91164ff772c6c4da72bc1
Mat Allen <mallen@mallen.redhat>
2014-08-12 16:26:37 +0100

    disabling logback because it can actually prevent deployments

--- acceptance/src/test/java/org/jboss/acceptance/steps/OrderServiceSteps.java
+++ acceptance/src/test/java/org/jboss/acceptance/steps/OrderServiceSteps.java
@@ -24,7 +24,7 @@ import cucumber.api.java.en.When;
 public class OrderServiceSteps{
   private static final Logger log=LoggerFactory.getLogger(OrderServiceSteps.class);
   
-  private static final String ORDER_SERVICE_URL="http://127.0.0.1:16080/order-service";
+  private static final String ORDER_SERVICE_URL="http://localhost:16080/order-service";
   private List<Order> orders=new ArrayList<Order>();
   
   // this doesnt work cos the cucumber api annotation causes an indexOutOfBoundsError - upgrade cukes?
