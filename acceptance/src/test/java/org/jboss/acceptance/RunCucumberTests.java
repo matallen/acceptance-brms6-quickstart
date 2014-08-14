96b8a9d3afa26dcaeca09bceb98e3de6413f906f
Mat Allen <mallen@mallen.redhat>
2014-08-14 15:50:31 +0100

    cucumber testing

--- acceptance/src/test/java/org/jboss/acceptance/RunCucumberTests.java
+++ acceptance/src/test/java/org/jboss/acceptance/RunCucumberTests.java
@@ -8,7 +8,12 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 //@Cucumber.Options(format = {/*"html:target/cucumber-html-report", */"json-pretty:target/cucumber-json-report.json"})
-@CucumberOptions(monochrome = true, format = {"pretty", "html:target/cucumber"})
+//@Cucumber.Options(features={"src/test/resources/cucumber"}, glue={"cucumber.com.zzz.yyy.steps"}, format={"html:target/test-reports"}, )
+
+@CucumberOptions(
+    monochrome = true, 
+    format = {"pretty", "html:target/cucumber", "json-pretty:target/cucumber/cucumber.json"}
+    )
 public class RunCucumberTests {
   @Test
   public void runAcceptanceTests(){}
