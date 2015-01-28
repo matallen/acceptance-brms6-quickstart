57887e89104723e18ab481fd8c7ab7bad5b49a6e
Mat Allen <mallen@mallen.redhat>
2015-01-28 14:16:06 -0600

    Upgraded to BPMS 6.0.3.GA

--- install/maven-install-business-central-only.sh
+++ install/maven-install-business-central-only.sh
@@ -6,7 +6,7 @@ URL="file:///home/$USER/.m2/repository"
 DISTRO=$1
 if [ "x$DISTRO" == "x" ]; then
   echo "Please pass distro zip as a parameter, ie:"
-  echo "  maven-install-business-central-only.sh jboss-bpms-6.0.2.GA-redhat-5-deployable-generic.zip"
+  echo "  maven-install-business-central-only.sh jboss-bpms-6.0.3.GA-redhat-1-deployable-generic.zip"
   exit
 fi
 
@@ -19,6 +19,8 @@ elif [[ $binaryVersion =~ 6.0.1.+ ]]; then
   internalVersion="6.0.2-redhat-6"
 elif [[ $binaryVersion =~ 6.0.2.+ ]]; then 
   internalVersion="6.0.3-redhat-4"
+elif [[ $binaryVersion =~ 6.0.3.+ ]]; then 
+  internalVersion="6.0.3-redhat-6"
 fi
 
 rm -rf tmp
